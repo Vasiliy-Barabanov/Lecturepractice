ca5b4151ca49e87b3745cbdc1b4a81f7b8ca187f
Vasiliy-Barabanov <72937599+Vasiliy-Barabanov@users.noreply.github.com>
2021-10-13 23:51:40 +0300

    Add files via upload

---
+++ Lectia6.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void foo(vector<int> vec, int n) {
+	try
+	{
+		vec.at(n);
+		cout << n << endl;
+		foo(vec, n + 1);
+	}
+	catch (...)
+	{
+		return;
+	}
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> vec(n);
+	foo(vec, 0);
+}
+
